0d06a686020118669bb800c22473db0465c0c735
monika-sivickaite <64109133+monika-sivickaite@users.noreply.github.com>
2020-04-21 19:12:33 -0600

    Add files via upload

---
+++ PC#00.py
@@ -0,0 +1,22 @@
+
+#PC00 - Hello world
+#Monika Sivickaite, Jamie Marriott
+# 20//01/17
+#
+# conversation 
+
+
+#create variable
+message = "Hello world"
+#prints our message
+print (message)
+#input function pauses program and waoth for user to enter some text
+name = input ("Hello, what is your name?:")
+#prints message with the name 
+print (f"\nHello, {name}!")
+
+
+
+
+
+
